68df68a8a7a1f2c5627c13179a9920f2ee63f2a2
Mohammed <ahmohammed@canada.root.corp>
2018-06-09 03:39:27 -0400

    fix name

--- src/sample.directive.ts
+++ src/sample.directive.ts
@@ -1,70 +1,60 @@
-  import * as interacter_ from 'interactjs';
-  let interacter: any = (<any>interacter_).default || interacter_;
+import * as interacter_ from 'interactjs';
+let interacter: any = (<any>interacter_).default || interacter_;
 
 import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[sampleDirective]'
+selector: '[sampleDirective]'
 })
 export class SampleDirective {
 
-  constructor(private el: ElementRef, renderer: Renderer) {
-    renderer.setElementProperty(el.nativeElement, 'innerHTML', 'some new value');
- }
+constructor(private el: ElementRef, renderer: Renderer) {
+  renderer.setElementProperty(el.nativeElement, 'innerHTML', 'some new vdddalue');
+}
 
- @HostListener('click')
-  ngOnInit2() {
-    const el = this.el.nativeElement;
-            // const arrow = document.getElementsByClassName('.container-fluid');
+@HostListener('dragstart')
+ngOnInit2() {
 
-        // target elements with the 'draggable' class
-        interacter(el)
+  const el = this.el.nativeElement;
+          // const arrow = document.getElementsByClassName('.container-fluid');
 
-          .draggable({
+      // target elements with the 'draggable' class
+      interacter(el)
 
-            // enable inertial throwing
-            inertia: true,
-            // keep the element within the area of it's parent
-            restrict: {
-              restriction: parent,
-              endOnly: true,
-              elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-            },
-            // enable autoScroll
-            autoScroll: true,
+        .draggable({
 
-            // call this function on every dragmove event
-            onmove: this.dragMoveListener,
-            // call this function on every dragend event
-            onend: function (event) {
-              const textEl = event.target.querySelector(el);
+          // enable inertial throwing
+          inertia: true,
+          // keep the element within the area of it's parent
+          restrict: {
+            restriction: parent,
+            endOnly: true,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+          },
+          // enable autoScroll
+          autoScroll: true,
 
-                // tslint:disable-next-line:no-bitwise
-              // tslint:disable-next-line:no-unused-expression
-              textEl && ( textEl.textContent =
-                'moved a distance of '
-                // tslint:disable-next-line:no-bitwise
-                + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                  Math.pow(event.pageY - event.y0, 2) | 0))
-                  .toFixed(2) + 'px');
-            }
-          });
-      }
+          // call this function on every dragmove event
+          onmove: this.dragMoveListener
+          // call this function on every dragend event
+        });
+    }
+   
+    
+    dragMoveListener(event) {
+      const target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-      dragMoveListener(event) {
-        const target = event.target,
-          // keep the dragged position in the data-x/data-y attributes
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      // translate the element
+      target.style.webkitTransform =
+        target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
 
-        // translate the element
-        target.style.webkitTransform =
-          target.style.transform =
-          'translate(' + x + 'px, ' + y + 'px)';
-
-        // update the posiion attributes
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      }
+      // update the posiion attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
 
 }
